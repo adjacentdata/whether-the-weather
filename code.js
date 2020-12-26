edd3e2ccbf67de7a9d48edaec7ad0c99cf242295
AJ Perez <72290134+albert3rd@users.noreply.github.com>
2020-12-26 23:12:01 +1000

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
-const search = document.querySelector('input');
-search.addEventListener('keypress', searchfinder);
-
 function searchfinder(event){
     if(event.keyCode == 13){
         console.log(search.value)
     }
 }
+
+const search = document.querySelector('input');
+search.addEventListener('keypress', searchfinder);
+
+
