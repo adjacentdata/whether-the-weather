2702fc75e77e9faec7a88b60c5cb2ca0c52bb781
AJ Perez <72290134+albert3rd@users.noreply.github.com>
2020-12-27 14:07:02 +1000

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,21 @@
-// Add your own API here... 
-// Store it in a variable. I am using weatherAPI. 
 function searchfinder(event){
     if(event.keyCode == 13){
         console.log(search.value)
     }
 }
 
+function result(returnedCity){
+    fetch(`${weatherAPI.baseurl}`)
+}
+
 const search = document.querySelector('input');
 search.addEventListener('keypress', searchfinder);
+
+function searchResults(searchedCity){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${searchedCity}&appid={"Your API Key here"}`)
+    .then(weatherData => {
+        return weatherData.json()
+    }).then(finalResults);
+}
 
 
